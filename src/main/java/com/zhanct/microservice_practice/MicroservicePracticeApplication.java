4e95a4c79c8256db8c08b689e0dc2219ac862ed5
Chuntao Zhang <31872379@qq.com>
2020-08-08 09:31:44 +0800

    first commit

---
+++ src/main/java/com/zhanct/microservice_practice/MicroservicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.zhanct.microservice_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicePracticeApplication.class, args);
+    }
+
+}
